a97873030e2f2913c8b7ef44c13909d3343fa583
exmaximum <utkualt06@outlook.com>
2022-11-27 12:32:15 +0300

    fix db

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,7 +9,8 @@ fn main() {
     Env::read_from_env(&format!("{}.env", &path));
     Env::check_secret_key();
 
-    let db = DbConnection::new(&(path + r"db\table.db")); //TODO
+    let db = DbConnection::new(&(path + r"db\table.db"));
+    db.create_password_table();
     let connection = db.get_connection();
     let args = Args::new();
     args.run(connection);
--- src/lib/config/db.rs
+++ src/lib/config/db.rs
@@ -11,6 +11,7 @@ pub struct DbConnection {
 impl DbConnection {
     pub fn new(path: &str) -> Self {
         let connection = DbConnection::check_db_location(path).unwrap();
+
         Self { connection }
     }
 
@@ -37,6 +38,11 @@ impl DbConnection {
                 Err(error.to_string())
             }
         }
+    }
+
+    pub fn create_password_table(&self) {
+        let statement = "CREATE TABLE IF NOT EXISTS \nPasswords (\n\tId\tINTEGER NOT NULL UNIQUE,\n\tName\tTEXT NOT NULL UNIQUE,\n\tPassword\tINTEGER NOT NULL,\n\tPRIMARY KEY(Id AUTOINCREMENT)\n)";
+        self.connection.prepare(statement).unwrap().next().unwrap();
     }
 
     fn create_dir_an_db() {
