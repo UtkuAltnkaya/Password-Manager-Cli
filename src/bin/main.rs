d9bf28ec40ba1f313ddb3a15204bbd5fe0f2fa02
exmaximum <utkualt06@outlook.com>
2022-11-27 09:50:17 +0300

    update-toml

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,18 @@
+use std::env;
+
 use password_manager::{
     args::args::Args,
     config::{db::DbConnection, dotenv},
 };
 
 fn main() {
+    let mut path = env::current_exe().unwrap().display().to_string();
     dotenv::read_from_env();
     dotenv::check_secret_key();
 
-    let db = DbConnection::new("db/table.db"); //TODO
+    path = (path.trim_end_matches("pm.exe")).to_string();
+    //&(path.to_string() + r"\db\table.db")
+    let db = DbConnection::new(&(path + r"\db\table.db")); //TODO
     let connection = db.get_connection();
     let args = Args::new();
     args.run(connection);
